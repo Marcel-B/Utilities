e833ec6773558ae243e689a63a20b0ecce73ce6b
Marcel Benders <marcel.benders@icloud.com>
2020-02-28 19:40:17 +0100

    (master) Add SecretProvider

---
+++ Utilities/Docker/SecretProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+
+namespace com.b_velop.Utilities.Docker
+{
+    public class SecretProvider
+    {
+        public string GetSecret(
+            string key)
+        {
+            const string DOCKER_SECRET_PATH = "/run/secrets/";
+            if (Directory.Exists(DOCKER_SECRET_PATH))
+            {
+                using var provider = new PhysicalFileProvider(DOCKER_SECRET_PATH);
+                var fileInfo = provider.GetFileInfo(key);
+                if (fileInfo.Exists)
+                {
+                    using var stream = fileInfo.CreateReadStream();
+                    using var streamReader = new StreamReader(stream);
+                    return streamReader.ReadToEnd();
+                }
+            }
+            return Environment.GetEnvironmentVariable(key);
+        }
+    }
+}
