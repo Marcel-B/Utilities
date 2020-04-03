6a8acd56e3e86b505bfd5334108d3120496ac750
Marcel Benders <marcel.benders@icloud.com>
2020-04-03 17:27:27 +0200

    (master) Add DateTimeExt, Update Version

---
+++ Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace com.b_velop.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToDateTime(
+            this int value)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(value).ToLocalTime();
+            return dtDateTime;
+        }
+    }
+}
